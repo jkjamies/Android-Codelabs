7e43f80f1761feb1f3f88d50221cc56394a0afe9
jkjamies <jkjamies@mtu.edu>
2022-07-10 21:46:22 -0400

    added test for navgating via ui click, added nav host setup before function

--- Jetpack Compose/NavigationCodelab/app/src/androidTest/java/com/example/compose/rally/RallyNavHostTest.kt
+++ Jetpack Compose/NavigationCodelab/app/src/androidTest/java/com/example/compose/rally/RallyNavHostTest.kt
@@ -3,8 +3,10 @@ package com.example.compose.rally
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.performClick
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
@@ -14,14 +16,28 @@ class RallyNavHostTest {
     val composeTestRule = createComposeRule()
     lateinit var navController: NavHostController
 
-    @Test
-    fun rallyNavHost() {
+    @Before
+    fun setupRallyNavHost() {
         composeTestRule.setContent {
             navController = rememberNavController()
             RallyNavHost(navController = navController)
         }
+    }
+
+    @Test
+    fun rallyNavHost() {
         composeTestRule
             .onNodeWithContentDescription("Overview Screen")
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun rallyNavHost_navigatesToAllAccounts_viaUI() {
+        composeTestRule
+            .onNodeWithContentDescription("All Accounts")
+            .performClick()
+        composeTestRule
+            .onNodeWithContentDescription("Accounts Screen")
             .assertIsDisplayed()
     }
 
